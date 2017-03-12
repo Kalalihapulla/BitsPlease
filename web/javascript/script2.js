c514186bd9a15879aeec5d0347857b060a8ba11d
Samu <Samu@88.195.147.253>
2017-03-12 21:50:45 +0200

    Mailbox almost working! Delete needs database implementation.

--- web/javascript/script2.js
+++ web/javascript/script2.js
@@ -206,11 +206,11 @@ function getMessages(email) {
                 bodyT = body[i].childNodes[0].nodeValue;
                 timeT = time[i].childNodes[0].nodeValue;
                 idT = id[i].childNodes[0].nodeValue;
-                var domElement = $("<tr data-toggle='collapse' data-target='#"+idT+"' class='accordion-toggle'>\n\
+                var domElement = $("<tr data-toggle='collapse' data-target='#mail"+i+"' class='accordion-toggle'>\n\
                         <td><button class='btn btn-default btn-sm' onclick='removeMessage($(this))'><i class='glyphicon glyphicon-trash'></i></button></td><td class='mailbox-name'>From: "+senderT+" - To: "+receiverT+"\n\
                         </td><td class='mailbox-subject'><b>Subject: "+subjectT+"</b></td>\n\
                         <td class='mailbox-date'>Time: "+timeT+"</td></tr><tr>\n\
-                        <td colspan='12' class='hiddenRow'><div class='accordian-body collapse' id='"+idT+"'><div>"+bodyT+"</div></td></tr>");
+                        <td colspan='12' class='hiddenRow'><div class='accordian-body collapse' id='mail"+i+"'><div>"+bodyT+"</div></td></tr>");
 
                 $('#mailtable').append(domElement);
 
@@ -232,7 +232,6 @@ function getMessages(email) {
 }
 
 function removeMessage(caller) {
-    console.log(caller);
     $(caller).closest('tr').remove();
 }
 
