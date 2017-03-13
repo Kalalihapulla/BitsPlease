67e2fb37bafdb4ad553b0066650dd6c567537fbc
himelr <rebelhaze@gmail.com>
2017-03-13 17:42:32 +0200

    Merge origin/main
    Conflicts:
    	web/javascript/script.js
    	web/javascript/script2.js
    	web/messages.jsp
    	web/profile.jsp
    	web/signup.jsp

--- web/javascript/script2.js
+++ web/javascript/script2.js
@@ -206,7 +206,7 @@ function getMessages(email) {
                 bodyT = body[i].childNodes[0].nodeValue;
                 timeT = time[i].childNodes[0].nodeValue;
                 idT = id[i].childNodes[0].nodeValue;
-                var domElement = $("<tr data-toggle='collapse' data-target='#mail"+i+"' class='accordion-toggle'>\n\
+                var domElement = $("<tr data-toggle='collapse' data-target='#mail"+idT+"' class='accordion-toggle'>\n\
                         <td><button class='btn btn-default btn-sm' onclick='removeMessage($(this))'><i class='glyphicon glyphicon-trash'></i></button></td><td class='mailbox-name'>From: "+senderT+" - To: "+receiverT+"\n\
                         </td><td class='mailbox-subject'><b>Subject: "+subjectT+"</b></td>\n\
                         <td class='mailbox-date'>Time: "+timeT+"</td></tr><tr>\n\
